13953e2f38cff401815a5f77fde1f9a112414b00
danielswaine <dt@aprilone.co.uk>
2015-06-30 19:49:09 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    getCurrentYear = Date.current.year
+    base_title = "Sodbury Challenge #{getCurrentYear}"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,23 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+
   getCurrentYear = Date.current.year
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Sodbury Challenge #{getCurrentYear}"
+    assert_select "title", "Sodbury Challenge #{getCurrentYear}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Sodbury Challenge #{getCurrentYear}"
+    assert_select "title", "About - Sodbury Challenge #{getCurrentYear}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Sodbury Challenge #{getCurrentYear}"
+    assert_select "title", "Help - Sodbury Challenge #{getCurrentYear}"
   end
 end
