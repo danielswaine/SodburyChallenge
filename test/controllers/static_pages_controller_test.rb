3337bbb6debd451242cd27553517929ea21845d0
danielswaine <dt@aprilone.co.uk>
2015-06-30 18:30:47 +0100

    Finish basic pages (home, about, help)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  getCurrentYear = Date.current.year
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Sodbury Challenge #{getCurrentYear}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Sodbury Challenge #{getCurrentYear}"
   end
 
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Sodbury Challenge #{getCurrentYear}"
+  end
 end
