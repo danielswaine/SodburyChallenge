30e10d186ae0023c02910f3850b289e7fa1be831
Joe Cridge <joe.cridge@me.com>
2015-09-29 12:24:09 +0100

    Simplify users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :user_logged_in?
+  before_action :find_user, only: [:edit, :update, :destroy]
+  before_action :correct_user?, only: [:edit, :update]
 
   def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
+    @users = User.all
   end
 
   def new
@@ -18,29 +14,27 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sodbury Challenge App!"
-      redirect_to @user
+      flash[:success] = "#{@user.name} has been added to the Sodbury Challenge app!"
+      redirect_to users_path
     else
       render 'new'
     end
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    old_name = @user.name
+    @user.destroy
+    flash[:success] = "#{old_name} has been removed."
+    redirect_to users_path
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
+      flash[:success] = "Account details updated."
+      redirect_to users_path
     else
       render 'edit'
     end
@@ -54,26 +48,21 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters
+    def find_user
+      @user = User.find(params[:id])
+    end
 
-    # Confirms a logged-in user.
-    def logged_in_user
+    def user_logged_in?
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "Please log in to manage users."
         redirect_to login_url
       end
     end
 
-    # Confirms the correct user.
-    def correct_user
+    def correct_user?
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # Confirms an admin user.
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
     end
 
 end
