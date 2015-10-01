ade4d03f4037eb6375b57fe6ace715553bb98605
Joe Cridge <joe.cridge@me.com>
2015-10-01 22:20:39 +0100

    Capistrano housekeeping didn't work; revert

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,11 +45,10 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      within release_path do
-        execute :rake, 'cache:clear'
-        execute :rake, 'db:migrate'
-        execute :rake, 'assets:precompile'
-      end
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
 
