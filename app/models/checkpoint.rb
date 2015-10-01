42d2b62cd460123b3915c14457387b5f60e8584e
Joe Cridge <joe.cridge@me.com>
2015-10-01 17:15:49 +0100

    Add checkpoint validations

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -1,7 +1,25 @@
 class Checkpoint < ActiveRecord::Base
 
-  validates :CheckpointID, presence: true, length: { maximum: 3 }, uniqueness: { case_sensitive: false }
-  validates :GridReference, presence: true, length: { maximum: 9 }
-  validates :CheckpointDescription, presence: true, length: { maximum: 255 }
+  validates :number, presence: true,
+                     uniqueness: true,
+                     numericality: {
+                                     greater_than_or_equal_to: 1,
+                                     only_integer: true
+                                   }
+
+  validates :grid_reference, presence: true
+
+  validates_each :grid_reference do |record, attr, value|
+    if value =~ /\A\d{4}-\d{4}\z/
+      if value =~ /\d{3}[1-46-9]/
+        record.errors.add(attr, 'parts must end in a 0 or a 5')
+      end
+    else
+      record.errors.add(attr, 'must look like 2345-7890')
+    end
+  end
+
+  validates :description, presence: true,
+                          length: { maximum: 375 }
 
 end
