c4daf983dbda533a3c40c946296b01118eec4d7c
Joe Cridge <joe.cridge@me.com>
2015-10-01 18:11:52 +0100

    Don't index users by email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001170638) do
+ActiveRecord::Schema.define(version: 20151001170928) do
 
   create_table "checkpoints", force: :cascade do |t|
     t.integer  "number"
@@ -46,7 +46,5 @@ ActiveRecord::Schema.define(version: 20151001170638) do
     t.string   "password_digest"
     t.string   "remember_digest"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
