674ed7dfb05aac159fd2aa07a69c29900b0767b7
Joe Cridge <joe.cridge@me.com>
2015-10-04 14:30:25 +0100

    Initial stab at teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,24 @@
 class Team < ActiveRecord::Base
   belongs_to :challenge, inverse_of: :teams
+
+  enum :group [:scouts, :explorers, :non_competitive]
+
+  serialize :visited, Array
+
+  before_save { self.name = name.titleize }
+
+  validates :group, presence: true
+
+  validates :name, length: {
+                             in: 5..70,
+                             too_short: 'is too short',
+                             too_long: 'is too long'
+                           },
+                   format: {
+                             with: /\A[ -~]*\z/,
+                             message: 'contains invalid characters'
+                           }
+
+  validates :nominal_start_time, presence: true
+
 end
