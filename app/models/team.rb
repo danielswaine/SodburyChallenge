252a98cbc2166286bc23ead5fca71698f84cf6a2
Joe Cridge <joe.cridge@me.com>
2015-10-04 20:35:26 +0100

    Mostly finished teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,14 +2,9 @@ class Team < ActiveRecord::Base
   belongs_to :challenge, inverse_of: :teams
 
   enum group: [:scouts, :explorers, :non_competitive]
-
   serialize :visited, Array
 
   before_save { self.name = name.titleize }
-
-  validates :challenge_id, presence: true
-
-  validates :group, presence: true
 
   validates :name, length: {
                              in: 5..70,
@@ -21,6 +16,26 @@ class Team < ActiveRecord::Base
                              message: 'contains invalid characters'
                            }
 
-  validates :nominal_start_time, presence: true
+  challenges_array = Challenge.all.map { |c| c.id }
+
+  validates :challenge_id, presence: true,
+                           inclusion: { in: challenges_array }
+
+  validates :group, presence: true,
+                    inclusion: { in: Team.groups.keys }
+
+  validates :nominal_start_time, presence: true,
+                                 uniqueness: true
+
+  validates_each :nominal_start_time, :actual_start_time,
+                 :phone_in_time, :finish_time do |record, attr, value|
+
+    unless value.nil?
+      unless value =~ /\A(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]\z/
+        record.errors.add(attr, 'must be a valid 24-hour time')
+      end
+    end
+
+  end
 
 end
