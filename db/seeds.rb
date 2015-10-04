4769da21bbbd8d5ea7a0260da3f319b53380ded5
Joe Cridge <joe.cridge@me.com>
2015-10-04 13:57:19 +0100

    Remove bangs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-User.create!(name:  "Test User",
-             email: "test@test.com",
-             password:              "testingtesting123",
-             password_confirmation: "testingtesting123",
-             admin: false)
+User.create(name:  "Test User",
+            email: "test@test.com",
+            password:              "testingtesting123",
+            password_confirmation: "testingtesting123",
+            admin: false)
 
-User.create!(name:  "Admin User",
-             email: "admin@admin.com",
-             password:              "adminadmin123",
-             password_confirmation: "adminadmin123",
-             admin: true)
+User.create(name:  "Admin User",
+            email: "admin@admin.com",
+            password:              "adminadmin123",
+            password_confirmation: "adminadmin123",
+            admin: true)
 
 50.times do |num|
-  Checkpoint.create!(number: num + 1,
-                     grid_reference: "2345-7890",
-                     description: "Example checkpoint number #{num + 1}")
+  Checkpoint.create(number: num + 1,
+                    grid_reference: "2345-7890",
+                    description: "Example checkpoint number #{num + 1}")
 end
