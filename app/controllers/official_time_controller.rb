ecdcd9da31fa643ddb353c0fbb124e5e31f57c62
Joe Cridge <joe.cridge@me.com>
2015-10-07 22:44:01 +0100

    Update time controller

--- app/controllers/official_time_controller.rb
+++ app/controllers/official_time_controller.rb
@@ -4,10 +4,12 @@ class OfficialTimeController < ApplicationController
     render json: time_object.to_json
   end
 
-  def time_object
-    zone = Time.now.gmtoff * 1000  # Server UTC offset in milliseconds
-    unix = (Time.now.to_f * 1000).round  # Server Unix time in milliseconds
-    { zone: zone, unix: unix }
-  end
+  private
+
+    def time_object
+      zone = Time.now.gmtoff * 1000  # Server UTC offset in milliseconds
+      unix = (Time.now.to_f * 1000).round  # Server Unix time in milliseconds
+      { zone: zone, unix: unix }
+    end
 
 end
