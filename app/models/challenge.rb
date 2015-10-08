39b28c05095a241440b12cb39f2b799a4e5a6d9f
Joe Cridge <joe.cridge@me.com>
2015-10-08 22:59:04 +0100

    Working bonus storage
    The bonus update action will eventually need to be separated from the RESTful
    update action to allow e.g. in-place editing of challenge attributes.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -14,14 +14,16 @@ class Challenge < ActiveRecord::Base
   validates_each :bonus_one, :bonus_two, :bonus_three,
                  :bonus_four, :bonus_five do |record, attr, value|
 
-    if value =~ /[^{ }a-z:,0-9\[\]]/
-      record.errors.add(attr, 'contains invalid characters')
-    elsif value =~ /\A\{ ?visit: ?[1-9][0-9]* ?, ?value: ?[1-9][0-9]* ?}\z/
-      # Valid bonus, based on total checkpoints visited.
-    elsif value =~ /\A\{ ?visit: ?\[ ?([1-9][0-9]*, ?)*[1-9][0-9]* ?\] ?, ?value: ?[1-9][0-9]* ?}\z/
-      # Valid bonus, based on visiting a specific subset of checkpoints.
-    else
-      record.errors.add(attr, 'does not have a valid format')
+    unless value.empty?
+      if value =~ /[^{ }a-z:,0-9\[\]]/
+        record.errors.add(attr, 'contains invalid characters')
+      elsif value =~ /\A\{ ?visit: ?[1-9][0-9]* ?, ?value: ?[1-9][0-9]* ?}\z/
+        # Valid bonus, based on total checkpoints visited.
+      elsif value =~ /\A\{ ?visit: ?\[ ?([1-9][0-9]*, ?)*[1-9][0-9]* ?\] ?, ?value: ?[1-9][0-9]* ?}\z/
+        # Valid bonus, based on visiting a specific subset of checkpoints.
+      else
+        record.errors.add(attr, 'does not have a valid format')
+      end
     end
 
   end
