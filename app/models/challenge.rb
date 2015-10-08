4c2cde5f8b10712c1a4403871cec53e5ab0a50b9
Joe Cridge <joe.cridge@me.com>
2015-10-08 22:34:08 +0100

    Add validations so bonuses

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,12 +3,6 @@ class Challenge < ActiveRecord::Base
   has_many :checkpoints, through: :goals
   has_many :teams, inverse_of: :challenge, dependent: :destroy
 
-  serialize :bonus_one, Hash
-  serialize :bonus_two, Hash
-  serialize :bonus_three, Hash
-  serialize :bonus_four, Hash
-  serialize :bonus_five, Hash
-
   validates :date, presence: true
 
   validates :time_allowed, numericality: {
@@ -16,4 +10,20 @@ class Challenge < ActiveRecord::Base
                                      only_integer: true,
                                      message: 'must be an integer number of hours'
                                    }
+
+  validates_each :bonus_one, :bonus_two, :bonus_three,
+                 :bonus_four, :bonus_five do |record, attr, value|
+
+    if value =~ /[^{ }a-z:,0-9\[\]]/
+      record.errors.add(attr, 'contains invalid characters')
+    elsif value =~ /\A\{ ?visit: ?[1-9][0-9]* ?, ?value: ?[1-9][0-9]* ?}\z/
+      # Valid bonus, based on total checkpoints visited.
+    elsif value =~ /\A\{ ?visit: ?\[ ?([1-9][0-9]*, ?)*[1-9][0-9]* ?\] ?, ?value: ?[1-9][0-9]* ?}\z/
+      # Valid bonus, based on visiting a specific subset of checkpoints.
+    else
+      record.errors.add(attr, 'does not have a valid format')
+    end
+
+  end
+
 end
