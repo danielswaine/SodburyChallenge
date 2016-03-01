0369008635921a11624fe8fc3e9de36f547b13ff
Daniel Swaine <info@danielswaine.co.uk>
2016-03-01 20:12:37 +0000

    Add start_point to Goals table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010120521) do
+ActiveRecord::Schema.define(version: 20160301195008) do
 
   create_table "challenges", force: :cascade do |t|
     t.date     "date"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151010120521) do
     t.boolean  "compulsory",    default: false
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.boolean  "start_point"
   end
 
   create_table "teams", force: :cascade do |t|
