fa38ec77e82326f68315de1233af898661e86971
Daniel Swaine <info@danielswaine.co.uk>
2016-03-02 20:56:47 +0000

    Generate random grid references

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,14 @@ User.create(name:  "Admin User",
 
 50.times do |num|
   description = Faker::Lorem.sentence
+  final_digit = ["0", "5"].sample
+  grid_reference_x = Faker::Number.number(3) + final_digit
+  grid_reference_y = Faker::Number.number(3) + final_digit
   Checkpoint.create(number: num + 1,
-                    grid_reference: "2839-1028",
-                    description: description
-                    )
+                    grid_reference: grid_reference_x + "-" + grid_reference_y,
+                    description: description)
 end
+
+p "Seed completed."
+p "#{Checkpoint.count} Checkpoints created."
+p "#{User.count} Users created."
