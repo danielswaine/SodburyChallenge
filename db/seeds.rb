a938caafde07ed8227ecd8f12d8fa94f6d32e6f9
Daniel Swaine <info@danielswaine.co.uk>
2016-03-02 21:26:34 +0000

    Improve seeds.rb syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,13 @@ User.create(name:  "Admin User",
             password_confirmation: "adminadmin123",
             admin: true)
 
+p "#{User.count} Users created."
+
 50.times do |num|
   description = Faker::Lorem.sentence
-  final_digit = ["0", "5"].sample
-  grid_reference_x = Faker::Number.number(3) + final_digit
-  grid_reference_y = Faker::Number.number(3) + final_digit
   Checkpoint.create(number: num + 1,
-                    grid_reference: grid_reference_x + "-" + grid_reference_y,
+                    grid_reference: "%04d-%04d" % [(5 * rand(2000)), (5 * rand(2000))],
                     description: description)
 end
 
-p "Seed completed."
 p "#{Checkpoint.count} Checkpoints created."
-p "#{User.count} Users created."
