b8810c03cd955f466a04870227678cdb70ee811a
Joe Cridge <joe.cridge@me.com>
2016-06-10 16:17:39 +0100

    Update users model to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,27 +19,36 @@ class User < ActiveRecord::Base
     end
   end
 
-  validates :email, uniqueness: { case_sensitive: false },
-                    length: {
-                              in: 6..320,
-                              too_short: 'address is too short',
-                              too_long: 'address is too long'
-                            }
+  validates(
+    :email,
+    presence: true,
+    length: {
+      in: 6..320,
+      too_short: 'is too short',
+      too_long: 'is too long'
+    },
+    uniqueness: {
+      case_sensitive: false,
+      message: 'is already registered'
+    }
+  )
 
   validates_each :email do |record, attr, value|
 
     # Form helper takes care of most of the RFC3696 complaince.
 
-    if value =~ /@.+\..+/
+    if value =~ /.{65,}@/
+      record.errors.add(attr, "has too many characters before the '@'")
+    elsif value =~ /@.+\..{2,}\z/
       if value =~ /\A\./
-        record.errors.add(attr, 'address cannot start with a period')
+        record.errors.add(attr, 'cannot start with a period')
       elsif value =~ /\.@/
-        record.errors.add(attr, 'address cannot contain \'.@\'')
+        record.errors.add(attr, 'cannot contain \'.@\'')
       elsif value =~ /\.{2,}/
-        record.errors.add(attr, 'address cannot contain consecutive periods')
+        record.errors.add(attr, 'cannot contain consecutive periods')
       end
     else
-      record.errors.add(attr, 'address must be fully qualified')
+      record.errors.add(attr, 'must be fully qualified')
     end
 
   end
