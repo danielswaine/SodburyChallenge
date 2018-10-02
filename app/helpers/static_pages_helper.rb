d67b7a8d34d3781caacaa90f85db7e1169950c8a
Daniel Swaine <info@danielswaine.co.uk>
2018-10-02 19:15:38 +0100

    Dynamic result section titles

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+
+  # Return a unique list of groups associated with the teams of a challenge
+  def group_list(teams)
+    teams.map{|c| c.group.capitalize}
+         .uniq
+         .to_sentence(two_words_connector: " & ", last_word_connector: " & ")
+  end
+
 end
