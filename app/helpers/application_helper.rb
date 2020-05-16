968238263f450e2bc6aa8ff7c53ccaddba339434
Daniel Swaine <info@danielswaine.co.uk>
2020-05-16 15:57:33 +0100

    Added tests for ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,12 @@
 module ApplicationHelper
-
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    getCurrentYear = Date.current.year
-    base_title = "Sodbury Challenge #{getCurrentYear}"
+    current_year = Date.current.year
+    base_title = "Sodbury Challenge #{current_year}"
     if page_title.to_s.empty?
       base_title
     else
-      page_title + " - " + base_title
+      page_title + ' - ' + base_title
     end
   end
-
 end
