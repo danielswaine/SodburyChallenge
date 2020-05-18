3206604fc3ddf4c2b5ffa052f0b04963696c8019
Daniel Swaine <info@danielswaine.co.uk>
2020-05-18 21:33:23 +0100

    Added goal model spec

---
+++ spec/models/goal_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Goal, type: :model do
+  describe 'associations' do
+    it { is_expected.to belong_to(:challenge) }
+
+    it { is_expected.to belong_to(:checkpoint) }
+  end
+
+  describe 'validations' do
+    subject { build_stubbed :goal }
+
+    it { is_expected.to validate_presence_of(:challenge_id) }
+
+    it { is_expected.to validate_presence_of(:checkpoint_id) }
+
+    context 'when given a new points value' do
+      it do
+        should validate_numericality_of(:points_value)
+          .is_greater_than_or_equal_to(0)
+          .only_integer
+          .with_message('must be a non-negative integer')
+      end
+    end
+  end
+end
