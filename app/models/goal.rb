28130f03e4dfcaa6d79cec21be5d455e0378db1f
Daniel Swaine <info@danielswaine.co.uk>
2020-10-27 21:14:49 +0000

    Updated goal specs

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,7 +3,15 @@ class Goal < ActiveRecord::Base
   belongs_to :checkpoint
 
   validates :challenge_id, presence: true
-  validates :checkpoint_id, presence: true
+
+  validates(
+    :checkpoint_id,
+    presence: true,
+    uniqueness: {
+      message: 'already exists on this challenge.',
+      scope: :challenge_id
+    }
+  )
 
   validates :points_value, presence: true
   validates(
@@ -15,4 +23,14 @@ class Goal < ActiveRecord::Base
     },
     if: 'points_value.present?'
   )
+
+  validate :goal_cannot_be_both_start_and_compulsory
+
+  private
+
+  def goal_cannot_be_both_start_and_compulsory
+    if start_point && compulsory
+      errors.add(:base, 'A goal cannot be both a start point and compulsory.')
+    end
+  end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -22,5 +22,38 @@ RSpec.describe Goal, type: :model do
           .with_message('must be a non-negative integer')
       end
     end
+
+    context 'when setting goal type' do
+      it 'should validate that it cannot be a start and compulsory' do
+        goal = build_stubbed(:goal, start_point: true, compulsory: true)
+        goal.valid?
+        expect(goal.errors[:base]).to eq(
+          ['A goal cannot be both a start point and compulsory.']
+        )
+      end
+    end
+
+    context 'when validating if checkpoint used before' do
+      it 'should validate that the goal has not been added previously' do
+        challenge = create(:challenge)
+        create(:goal, challenge: challenge)
+        new_goal = build_stubbed(:goal, checkpoint_id: 1, challenge: challenge)
+        new_goal.valid?
+        expect(new_goal.errors[:checkpoint_id]).to eq(
+          ['already exists on this challenge.']
+        )
+      end
+
+      it 'should allow goal even if used in another challenge' do
+        old_challenge = create(:challenge)
+        create(:goal, challenge: old_challenge)
+        new_challenge = create(:challenge)
+        new_goal = build_stubbed(
+          :goal, checkpoint_id: 1, challenge: new_challenge
+        )
+        new_goal.valid?
+        expect(new_goal.errors[:checkpoint_id]).to eq([])
+      end
+    end
   end
 end
