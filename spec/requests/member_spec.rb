78cc36707aa876ff7e7da2bbee33004082a01563
Daniel Swaine <info@danielswaine.co.uk>
2021-01-04 19:39:46 +0000

    Modify string to class

--- spec/requests/member_spec.rb
+++ spec/requests/member_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Member', type: :request do
+RSpec.describe Member, type: :request do
   let(:user) { create(:user) }
   let(:team) { create(:team) }
   let(:member) { create(:member, team: team) }
