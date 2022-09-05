0ada4583803a0e5b10a5f21e9b2dbe9bb54bfe44
Daniel Swaine <info@danielswaine.co.uk>
2022-09-05 12:08:13 +0100

    Fixes #34 - handle joint scores

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,72 @@
 module StaticPagesHelper
-
   # Return a unique list of groups associated with the teams of a challenge
   def group_list(teams)
-    teams.map{|c| c.group.capitalize}
+    teams.map { |c| c.group.capitalize }
          .uniq
-         .to_sentence(two_words_connector: " & ", last_word_connector: " & ")
+         .to_sentence(two_words_connector: ' & ', last_word_connector: ' & ')
   end
 
+  # Return a result table that handles order, equal scores and lateness
+  def results_table(teams)
+    results = []
+    sorted = teams.order(:score).reverse.group_by(&:score)
+    position = 1
+
+    sorted.each do |_score, arr|
+      if arr.size > 1 # More than one team has the same score.
+        sorted_lateness = arr.sort_by { |t| lateness_in_minutes(expected_finish_time(t), t.finish_time) }
+        minutes_late = lateness_in_minutes(expected_finish_time(arr.first), arr.first.finish_time)
+
+        sorted_lateness.each_with_index do |team, index|
+          # All teams finished at the same time, so have equal scores.
+          if arr.all? { |a| lateness_in_minutes(expected_finish_time(a), a.finish_time) == minutes_late }
+            results.push(
+              {
+                position: "#{position.ordinalize}=",
+                name: team.name,
+                score: team.score,
+                minutes_late: lateness_in_minutes(expected_finish_time(team), team.finish_time)
+              }
+            )
+            position += 1 if arr.size - 1 == index
+          else
+            # Teams finished at different times.
+            results.push(
+              {
+                position: position.ordinalize,
+                name: team.name,
+                score: team.score,
+                minutes_late: lateness_in_minutes(expected_finish_time(team), team.finish_time)
+              }
+            )
+            position += 1
+          end
+        end
+      else
+        results.push(
+          {
+            position: position.ordinalize,
+            name: arr.first.name,
+            score: arr.first.score,
+            minutes_late: nil
+          }
+        )
+        position += 1
+      end
+    end
+
+    results
+  end
+
+  def format_minutes_late(minutes)
+    if minutes.nil?
+      ''
+    elsif minutes < 0
+      "(finished #{minutes.abs} minutes early)"
+    elsif minutes == 0
+      '(finished exactly on time)'
+    elsif minutes > 0
+      "(finished #{minutes.abs} minutes late)"
+    end
+  end
 end
--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include TeamsHelper
 
 RSpec.describe StaticPagesHelper, type: :helper do
   describe 'group_list' do
@@ -27,6 +28,111 @@ RSpec.describe StaticPagesHelper, type: :helper do
 
         expect(group_list(challenge.teams)).to eq('Explorers, Scouts & Leaders')
       end
+    end
+  end
+
+  describe 'results_table' do
+    context 'when teams finished early with no equal scores' do
+      it 'should return ordered array of scores' do
+        t1 = create(:team, actual_start_time: '18:00', finish_time: '22:59', score: 1)
+        t2 = create(:team, actual_start_time: '18:05', finish_time: '23:03', score: 2)
+        t3 = create(:team, actual_start_time: '18:10', finish_time: '23:07', score: 3)
+        teams = Team.all
+
+        expect(results_table(teams)).to eq(
+          [
+            { position: '1st', name: t3.name, score: 3, minutes_late: nil },
+            { position: '2nd', name: t2.name, score: 2, minutes_late: nil },
+            { position: '3rd', name: t1.name, score: 1, minutes_late: nil }
+          ]
+        )
+      end
+    end
+
+    context 'when teams finished early with same score and time' do
+      it 'should return ordered array of scores with joint position' do
+        t1 = create(:team, actual_start_time: '18:00', finish_time: '22:59', score: 3)
+        t2 = create(:team, actual_start_time: '18:05', finish_time: '23:04', score: 3)
+        t3 = create(:team, actual_start_time: '18:10', finish_time: '23:09', score: 1)
+        teams = Team.all
+
+        expect(results_table(teams)).to eq(
+          [
+            { position: '1st=', name: t2.name, score: 3, minutes_late: -1 },
+            { position: '1st=', name: t1.name, score: 3, minutes_late: -1 },
+            { position: '2nd',  name: t3.name, score: 1, minutes_late: nil }
+          ]
+        )
+      end
+    end
+
+    context 'when teams finished with same score but different times' do
+      it 'should return ordered array of scores with joint position' do
+        t1 = create(:team, actual_start_time: '18:00', finish_time: '23:05', score: 3)
+        t2 = create(:team, actual_start_time: '18:05', finish_time: '23:11', score: 3)
+        t3 = create(:team, actual_start_time: '18:10', finish_time: '23:17', score: 1)
+        teams = Team.all
+
+        expect(results_table(teams)).to eq(
+          [
+            { position: '1st', name: t1.name, score: 3, minutes_late: 5   },
+            { position: '2nd', name: t2.name, score: 3, minutes_late: 6   },
+            { position: '3rd', name: t3.name, score: 1, minutes_late: nil }
+          ]
+        )
+      end
+    end
+
+    context 'when teams finished on time with same score and time' do
+      it 'should return ordered array of scores with joint position' do
+        t1 = create(:team, actual_start_time: '18:00', finish_time: '23:00', score: 3)
+        t2 = create(:team, actual_start_time: '18:05', finish_time: '23:05', score: 1)
+        t3 = create(:team, actual_start_time: '18:10', finish_time: '23:10', score: 1)
+        teams = Team.all
+
+        expect(results_table(teams)).to eq(
+          [
+            { position: '1st',  name: t1.name, score: 3, minutes_late: nil },
+            { position: '2nd=', name: t3.name, score: 1, minutes_late: 0   },
+            { position: '2nd=', name: t2.name, score: 1, minutes_late: 0   }
+          ]
+        )
+      end
+    end
+
+    context 'when teams finished late (within 30 min allowance)' do
+      it 'should return ordered array of scores with joint position' do
+        t1 = create(:team, actual_start_time: '18:00', finish_time: '23:05', score: 3)
+        t2 = create(:team, actual_start_time: '18:05', finish_time: '23:10', score: 1)
+        t3 = create(:team, actual_start_time: '18:10', finish_time: '23:15', score: 1)
+        teams = Team.all
+
+        expect(results_table(teams)).to eq(
+          [
+            { position: '1st',  name: t1.name, score: 3, minutes_late: nil },
+            { position: '2nd=', name: t3.name, score: 1, minutes_late: 5   },
+            { position: '2nd=', name: t2.name, score: 1, minutes_late: 5   }
+          ]
+        )
+      end
+    end
+  end
+
+  describe 'format_minutes_lates' do
+    context 'when minutes nil' do
+      it { expect(format_minutes_late(nil)).to eq('') }
+    end
+
+    context 'when a teams finishes early' do
+      it { expect(format_minutes_late(-5)).to eq('(finished 5 minutes early)') }
+    end
+
+    context 'when a teams finishes on time' do
+      it { expect(format_minutes_late(0)).to eq('(finished exactly on time)') }
+    end
+
+    context 'when a teams finishes late' do
+      it { expect(format_minutes_late(5)).to eq('(finished 5 minutes late)') }
     end
   end
 end
